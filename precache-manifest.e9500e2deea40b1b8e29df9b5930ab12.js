57e9b44957f15948f0f160b104c8873415267b44
SukSerhiy <sukserhiy@gmail.com>
2019-03-04 15:36:48 +0200

    Updates

---
+++ precache-manifest.e9500e2deea40b1b8e29df9b5930ab12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/alpha-bank-test/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/alpha-bank-test/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "2131b0223b667a7f5874",
+    "url": "/alpha-bank-test/static/js/runtime~main.2131b022.js"
+  },
+  {
+    "revision": "963a31c18d41e466ce06",
+    "url": "/alpha-bank-test/static/js/main.963a31c1.chunk.js"
+  },
+  {
+    "revision": "1d91787810b690a8a4a5",
+    "url": "/alpha-bank-test/static/js/2.1d917878.chunk.js"
+  },
+  {
+    "revision": "963a31c18d41e466ce06",
+    "url": "/alpha-bank-test/static/css/main.c89e8f55.chunk.css"
+  },
+  {
+    "revision": "1d91787810b690a8a4a5",
+    "url": "/alpha-bank-test/static/css/2.5e74e6be.chunk.css"
+  },
+  {
+    "revision": "9f59270a67d6cb1ab844f81cb148348d",
+    "url": "/alpha-bank-test/index.html"
+  }
+];
